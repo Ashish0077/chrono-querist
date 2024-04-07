25d51449af02c7fb6d3c03d726c4915a7b621443
Ashish Arora <ashish007722@gmail.com>
2024-04-07 15:19:22 +0530

    feat: putting ports and adapter together in main
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"chrono-querist/internal/adapters/primary/web"
+	"chrono-querist/internal/adapters/secondary/database/mysql"
+	"chrono-querist/internal/core/services/videos"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+
+	// Initialize the secondary adapters (Port Implementations)
+	videosRepo := mysql.NewVideoRepository()
+	fmt.Println("Video Repo done")
+
+	// Initialize the core service layer
+	// Business Logic is handled here
+	searchService := videos.NewService(videosRepo)
+	fmt.Println("Search Service done")
+
+	// Init primary adapters (Port Implementations)
+	srv := web.NewApp(searchService, web.WithPort(9000))
+	fmt.Println("Web Server done")
+
+	srv.Run()
 }
